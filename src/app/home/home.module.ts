2b47e9a3479f46a7f77e54eae0dfae330f129023
Ruben Rubio <Ruben.Rubio@ust.com>
2023-10-05 22:16:57 +0200

    feat: home component & module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+
+@NgModule({
+  imports: [IonicModule, CommonModule, FormsModule, HomePageRoutingModule],
+  declarations: [HomePage, DropdownComponent],
+})
+export class HomePageModule {}
